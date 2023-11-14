cc007e87f5f64840aad4bff8df2e23aaafba8209
Kim <xpa1234567@gmail.com>
2023-11-14 19:35:23 +0800

    報表統計頁面修改

--- api/api.py
+++ api/api.py
@@ -293,19 +293,15 @@ def acupoint():
     return render_template("acupoint.html", user=current_user.name, data=returnData)
 
 
-from flask import render_template, Blueprint
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from link import *
-# from api.sql import Analysis
+analysis = Blueprint("analysis", __name__, template_folder="../templates")
 
-analysis = Blueprint('analysis', __name__, template_folder='../templates')
 
-@api.route('/dashboard')
+@api.route("/dashboard")
 @login_required
 def dashboard():
     revenue = []
     dataa = []
-    for i in range(1,13):
+    for i in range(1, 13):
         row = Analysis.month_price(i)
 
         if not row:
@@ -313,44 +309,49 @@ def dashboard():
         else:
             for j in row:
                 revenue.append(j[1])
-        
+
         row = Analysis.month_count(i)
-        
+
         if not row:
             dataa.append(0)
         else:
             for k in row:
                 dataa.append(k[1])
-        
+
     row = Analysis.category_sale()
     datab = []
     for i in row:
-        temp = {
-            'value': i[0],
-            'name': i[1]
-        }
+        temp = {"value": i[0], "name": i[1]}
         datab.append(temp)
-    
+
     row = Analysis.member_sale()
-    
+
     datac = []
     nameList = []
     counter = 0
-    
+
     for i in row:
         counter = counter + 1
         datac.append(i[0])
     for j in row:
         nameList.append(j[2])
-    
+
     counter = counter - 1
-    
+
     row = Analysis.member_sale_count()
     countList = []
-    
+
     for i in row:
         countList.append(i[0])
-        
-    return render_template('dashboard.html', counter = counter, revenue = revenue, dataa = dataa, datab = datab, datac = datac, nameList = nameList, countList = countList)
 
-
+    return render_template(
+        "dashboard.html",
+        counter=counter,
+        revenue=revenue,
+        dataa=dataa,
+        datab=datab,
+        datac=datac,
+        nameList=nameList,
+        countList=countList,
+        user=current_user.name,
+    )
